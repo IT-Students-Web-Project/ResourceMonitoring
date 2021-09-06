d6c256ba0ef956351ff8214dc60e47d15723dd33
Fariusz <r.loth@icloud.com>
2021-09-06 20:01:43 +0200

    Splited getCpuLoad function to files

--- Klient/getCpuLoad.cpp
+++ Klient/getCpuLoad.cpp
@@ -1,8 +1,5 @@
-// Klient.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "getCpuLoad.hpp"
 
-#include <Windows.h>
-#include <iostream>
 
 static float CalculateCPULoad(unsigned long long idleTicks, unsigned long long totalTicks)
 {
@@ -29,25 +26,3 @@ float GetCPULoad()
     FILETIME idleTime, kernelTime, userTime;
     return GetSystemTimes(&idleTime, &kernelTime, &userTime) ? CalculateCPULoad(FileTimeToInt64(idleTime), FileTimeToInt64(kernelTime) + FileTimeToInt64(userTime)) : -1.0f;
 }
-
-int main()
-{
-
-    while (true) {
-        std::cout << GetCPULoad();
-        std::cout << std::endl;
-        Sleep(250);
-    }
-
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ Klient/getCpuLoad.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <Windows.h>
+
+#ifndef getCpuLoad_hpp
+#define getCpuLoad_hpp
+
+// Returns 1.0f for "CPU fully pinned", 0.0f for "CPU idle", or somewhere in between
+// You'll need to call this at regular intervals, since it measures the load between
+// the previous call and the current one.  Returns -1.0 on error.
+float GetCPULoad();
+static float CalculateCPULoad(unsigned long long idleTicks, unsigned long long totalTicks);
+static unsigned long long FileTimeToInt64(const FILETIME& ft);
+
+#endif
