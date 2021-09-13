83b1a6db0615b40a0bda880b98dd6e1fa323ea1b
Jacek Barc <jacebar970@student.polsl.pl>
2021-09-13 18:58:47 +0200

    Added charts

--- Server/selectedclient.cpp
+++ Server/selectedclient.cpp
@@ -19,6 +19,35 @@ SelectedClient::SelectedClient(Resources* r) : QDialog(nullptr),
     ui->labelDisk->setText(QString::number(resources->getDiskFreeSpacePercentage())+ "%");
     ui->labelMemory->setText(QString::number(resources->getTotalMemory()));
     ui->labelUsedMemory->setText(QString::number(1.0*resources->getMemoryLoad()/resources->getTotalMemory()) + "%");
+    int iter = 1;
+    for (float cpuLoad : resources->getCpuLoadList()){
+        lineSeries[0]->append(iter, cpuLoad);
+        iter++;
+    }
+    iter = 1;
+    for (float diskSpace : resources->getDiskFreeSpacePercentageList()){
+        lineSeries[1]->append(iter, diskSpace);
+        iter++;
+    }
+    iter = 1;
+    for (float memoryLoad : resources->getMemoryLoadListReference()){
+        lineSeries[2]->append(iter, 1.0 * memoryLoad/resources->getTotalMemory());
+        iter++;
+    }
+    charts[0]->setTitle("Cpu load");
+    charts[1]->setTitle("Free disk space");
+    charts[2]->setTitle("Free memory");
+    for (int i = 0; i < 3; i++) {
+        charts[i]->addSeries(lineSeries[i]);
+        charts[i]->legend()->hide();
+        charts[i]->createDefaultAxes();
+    }
+    ui->graphicsView_chart0->setChart(charts[0]);
+    ui->graphicsView_chart1->setChart(charts[1]);
+    ui->graphicsView_chart2->setChart(charts[2]);
+    ui->graphicsView_chart0->setRenderHint(QPainter::Antialiasing);
+    ui->graphicsView_chart1->setRenderHint(QPainter::Antialiasing);
+    ui->graphicsView_chart2->setRenderHint(QPainter::Antialiasing);
 }
 
 SelectedClient::~SelectedClient()
